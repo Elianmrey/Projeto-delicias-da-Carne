8c9de6f597c46d77f5580b80ff362c6f15be4be0
Elian M Reyes <elian060893@gmail.com>
2023-05-26 20:01:20 -0300

    Confirm Settings

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
 const radbuttonSelected = document.getElementsByClassName("radio-buton-selected");
 console.log(radbuttonSelected.length);
 
-// function selectRadioButton() {
-// let counter = 0
-// let validator = counter%2==0;
-
-// for (let count in radbuttonSelected)
-// {
-//     if(validator)
-//     {
-//         radbuttonSelected[count].style.display = "block";
-//     } else
-//     {
-//         radbuttonSelected[count].style.display = "none";
-//     }
-//     counter++;
-//  }
-    
-// };
-
-
-// radbuttonSelected.forEach(radbuttonSelected.addEventListener("click",selectRadioButton))
